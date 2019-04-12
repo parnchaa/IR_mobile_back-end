222313c38638a06a44f4554b5ea328f1b1390fb4
Unknown <kittisak.bas@mail.kmutt.ac.th>
2019-04-12 16:56:56 +0700

    [ADD] Get car infomation by licenseplate & get privilages by location

--- ir_parking_mobile_app_backend/ir_parking_db_server.js
+++ ir_parking_mobile_app_backend/ir_parking_db_server.js
@@ -1,6 +1,12 @@
 var mySQL = require('mysql')
 var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
 var app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 var con = mySQL.createConnection({
   host: 'localhost',
@@ -8,16 +14,33 @@ var con = mySQL.createConnection({
   password: '',
   database: 'ir_parking'
 })
-con.connect(function (err) {
+
+con.connect(err => {
   if (err) throw err
-  console.log('Connected!')
+  app.listen(3000, () => {
+    console.log('Start server at port 3000.')
+  })
 })
 
-app.get('/carinfo', function (request, response) {
-  var viewCarInfo= 'SELECT c.licensePlate,CONCAT(co.carOwnerFirstName," ",co.carOwnerLastName) As carOwnerName, co.carOwnerTel FROM carowners co JOIN car c ON co.carOwnerID = c.carOwnerID'
+app.get('/carinfo/:licenseplate', function (request, response) {
+  var key = request.params.licenseplate
+  console.log(key);
+  var viewCarInfo = 'SELECT c.licensePlate,CONCAT(co.carOwnerFirstName," ",co.carOwnerLastname) AS carOwnerName,co.carOwnerTel' + '\n' +
+    'FROM carowners co JOIN car c ON co.carOwnerID = c.carOwnerID WHERE c.licensePlate LIKE '+ mySQL.escape('%' + key +'%')
   con.query(viewCarInfo, function (err, result) {
     if (err) throw err
     response.send(result)
   })
 })
-app.listen(3000)
+
+app.get('/privilages/:location' , function (request,response) {
+  var key = request.params.location
+  console.log(key);
+  var viewPrivilagesOfcar = 'SELECT c.licensePlate, s.typeOfSticker,s.colorOfSticker, l.locationCode, l.locationName ' + '\n' + 
+    'FROM car c JOIN sticker s ON c.stickerID = s.stickerID JOIN location l ON s.stickerID = l.stickerID' + '\n' +
+    'WHERE l.locationName LIKE' + mySQL.escape('%' + key + '%')
+  con.query(viewPrivilagesOfcar, function (err, result) {
+    if(err) throw err
+    response.send(result)
+  })
+})
