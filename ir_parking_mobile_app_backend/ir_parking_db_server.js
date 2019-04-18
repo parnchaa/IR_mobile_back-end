9ecc20a8514f176fd7503f7b225eed91892ce64a
Unknown <kittisak.bas@mail.kmutt.ac.th>
2019-04-18 18:18:17 +0700

    [ADD]  get location and car for check privileges

--- ir_parking_mobile_app_backend/ir_parking_db_server.js
+++ ir_parking_mobile_app_backend/ir_parking_db_server.js
@@ -9,9 +9,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 var con = mySQL.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: '54.251.180.67',
+  user: 'parn',
+  password: 'irdb2019',
   database: 'ir_parking'
 })
 
@@ -25,22 +25,26 @@ con.connect(err => {
 app.get('/carinfo/:licenseplate', function (request, response) {
   var key = request.params.licenseplate
   console.log(key);
-  var viewCarInfo = 'SELECT c.licensePlate,CONCAT(co.carOwnerFirstName," ",co.carOwnerLastname) AS carOwnerName,co.carOwnerTel' + '\n' +
-    'FROM carowners co JOIN car c ON co.carOwnerID = c.carOwnerID WHERE c.licensePlate LIKE '+ mySQL.escape('%' + key +'%')
+  var viewCarInfo = 'SELECT c.licensePlate,CONCAT(co.carOwnerFirstName," ",co.carOwnerLastname) AS carOwnerName' + '\n' +
+     ',co.carOwnerTel FROM carowners co JOIN car c ON co.carOwnerID = c.carOwnerID ' + '\n' +
+     'WHERE c.licensePlate LIKE '+ mySQL.escape('%' + key +'%')
   con.query(viewCarInfo, function (err, result) {
     if (err) throw err
     response.send(result)
   })
 })
 
-app.get('/privilages/:location' , function (request,response) {
-  var key = request.params.location
-  console.log(key);
-  var viewPrivilagesOfcar = 'SELECT c.licensePlate, s.typeOfSticker,s.colorOfSticker, l.locationCode, l.locationName ' + '\n' + 
-    'FROM car c JOIN sticker s ON c.stickerID = s.stickerID JOIN location l ON s.stickerID = l.stickerID' + '\n' +
-    'WHERE l.locationName LIKE' + mySQL.escape('%' + key + '%')
+app.get('/privilages/:location/:licenseplate' , function (request,response) {
+  var keyLocation = request.params.location
+  var keyLicensePlate = request.params.licenseplate
+  console.log('Location: ',keyLocation);
+  console.log('LicensePlate: ', keyLicensePlate);
+  var viewPrivilagesOfcar = 'SELECT c.licensePlate, l.locationCode, l.locationName FROM car c JOIN ' + '\n' + 
+    'location l ON c.stickerID = l.stickerID WHERE l.locationName LIKE' + '\n' + mySQL.escape('%' + keyLocation + '%') + '\n' +
+    'AND c.licensePlate LIKE' + mySQL.escape('%' + keyLicensePlate + '%')
   con.query(viewPrivilagesOfcar, function (err, result) {
     if(err) throw err
     response.send(result)
   })
 })
+
