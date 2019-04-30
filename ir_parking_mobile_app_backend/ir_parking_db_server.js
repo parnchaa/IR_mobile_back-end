4061573017896d9f7b44acd25d580c49275f3b29
Unknown <kittisak.bas@mail.kmutt.ac.th>
2019-04-30 11:18:27 +0700

    [ADD] insert problems fix data

--- ir_parking_mobile_app_backend/ir_parking_db_server.js
+++ ir_parking_mobile_app_backend/ir_parking_db_server.js
@@ -1,12 +1,21 @@
 var mySQL = require('mysql')
 var express = require('express')
+// var session = require('express-session')
+// var path = require('path')
 var cors = require('cors')
-var bodyParser = require('body-parser')
+var body = require('body-parser')
 var app = express()
 
+// app.use(session({
+//   secret: 'secret',
+//   resave: true,
+//   saveUninitialized: true
+// }))
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(body())
+app.use(body.urlencoded({ extended: true }))
+app.use(body.json())
+
 
 var con = mySQL.createConnection({
   host: '54.251.180.67',
@@ -26,7 +35,7 @@ app.get('/carinfo/:licenseplate', function (request, response) {
   var key = request.params.licenseplate
   console.log(key);
   var viewCarInfo = 'SELECT c.licensePlate,CONCAT(co.carOwnerFirstName," ",co.carOwnerLastname) AS carOwnerName' + '\n' +
-     ',co.carOwnerTel FROM carowners co JOIN car c ON co.carOwnerID = c.carOwnerID ' + '\n' +
+     ',co.carOwnerTel FROM CarOwners co JOIN Car c ON co.carOwnerID = c.carOwnerID ' + '\n' +
      'WHERE c.licensePlate LIKE '+ mySQL.escape('%' + key +'%')
   con.query(viewCarInfo, function (err, result) {
     if (err) throw err
@@ -34,17 +43,41 @@ app.get('/carinfo/:licenseplate', function (request, response) {
   })
 })
 
-app.get('/privilages/:location/:licenseplate' , function (request,response) {
+app.get('/privileges/:location/:licenseplate' , function (request,response) {
   var keyLocation = request.params.location
   var keyLicensePlate = request.params.licenseplate
   console.log('Location: ',keyLocation);
   console.log('LicensePlate: ', keyLicensePlate);
-  var viewPrivilagesOfcar = 'SELECT c.licensePlate, l.locationCode, l.locationName FROM car c JOIN ' + '\n' + 
-    'location l ON c.stickerID = l.stickerID WHERE l.locationName LIKE' + '\n' + mySQL.escape('%' + keyLocation + '%') + '\n' +
+  var viewPrivilegesOfcar = 'SELECT c.licensePlate, l.locationCode, l.locationName FROM Car c JOIN ' + '\n' + 
+    'Location l ON c.stickerID = l.stickerID WHERE l.locationName LIKE' + '\n' + mySQL.escape('%' + keyLocation + '%') + '\n' +
     'AND c.licensePlate LIKE' + mySQL.escape('%' + keyLicensePlate + '%')
-  con.query(viewPrivilagesOfcar, function (err, result) {
+  con.query(viewPrivilegesOfcar, function (err, result) {
     if(err) throw err
     response.send(result)
   })
 })
+
+app.get('/allegation' ,function(request,response){
+  var viewAllegation = "SELECT p.allegation AS value FROM ProblemType p"
+  con.query(viewAllegation,function (err,result) {
+    if(err) throw err
+    response.send(result)
+  })
+})
+
+
+app.post('/problems', function (request, response) {
+  var keyScene = request.body.scene
+  var keyProblemDetails = request.body.problemDetails
+  console.log("Scene: ", keyScene);
+  console.log("problemDetails: ", keyProblemDetails);
+  var insertProblems = "INSERT INTO Problems(scene,evidenceImage,problemDetails,dateOfProblem,timeOfProblem,ticketID,problemTypeID,staffID)" + '\n' +
+    "VALUES(" + mySQL.escape(keyScene) + ", null," + mySQL.escape(keyProblemDetails) + ", '2019-04-24', '18:00:00', 3, 1, 2)"
+  con.query(insertProblems,function (err, result) {
+    if (err) throw err
+    response.json(result)
+  })
+})
+
+
 
